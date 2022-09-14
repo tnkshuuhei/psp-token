d858024e1ad57160ff6dac00c83c6012ae13da25
田中修平 <tanakashuuhei@tanakashuuheinoMacBook-Pro.local>
2022-09-15 01:27:36 +0900

    psp32

---
+++ my_psp34/lib.rs
@@ -0,0 +1,42 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![feature(min_specialization)]
+
+#[openbrush::contract]
+pub mod my_psp34 {
+    use ink_storage::traits::SpreadAllocate;
+    use openbrush::{
+        contracts::psp34::extensions::metadata::*,
+        traits::Storage,
+    };
+
+    #[ink(storage)]
+    #[derive(Default, /*SpreadAllocate,*/ Storage)]
+    pub struct Contract {
+        #[storage_field]
+        psp34: psp34::Data,
+        #[storage_field]
+        metadata: Data,
+        next_id: u8,
+    }
+
+    impl PSP34 for Contract {}
+
+    impl Contract {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            ink_lang::codegen::initialize_contract(|_instance: &mut Self| {})
+        }
+
+        #[ink(message)]
+        pub fn mint_token(&mut self) -> Result<(), PSP34Error> {
+            self._mint_to(Self::env().caller(), Id::U8(self.next_id))?;
+            self.next_id += 1;
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn mint(&mut self, id: Id) -> Result<(), PSP34Error> {
+            self._mint_to(Self::env().caller(), id)
+        }
+    }
+}
